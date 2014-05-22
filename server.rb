db815e7eb33ec1a4d1b9a2a7bf00b966c67f8ba1
ZacharyAdams <zaekadams@gmail.com>
2014-05-22 15:06:53 -0400

    did all kings of things

--- server.rb
+++ server.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
+require 'csv'
+require 'pry'
+
+def read_in_art
+  articles = []
+  CSV.foreach('views/data.csv' , headers: true) do |row|
+    articles << row.to_hash
+  end
+  articles.reverse
+end
+
+
+
 
 
 get '/' do
+  @read = read_in_art
   erb :index
 end
 
@@ -15,14 +29,14 @@ get '/article_new' do
 end
 
 
-post '/article_new' do
+post '/article_new/apple' do
 
   article = params["article"]
   url = params["article_url"]
   source = params["source"]
-
-  CSV.open("data.csv", "a") do |csv|
-    csv << [article, url, source]
+  description = params["description"]
+  CSV.open("views/data.csv", "a") do |csv|
+    csv.puts([article,url,source,description])
   end
   redirect '/'
 end
